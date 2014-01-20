0b79fa08cb67132f47813769c22c04f8b719ccd7
paul1453 <hello@riosunrise.com>
2014-01-20 20:49:53 +0000

    fix the node REPL

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,7 +18,7 @@
 (require 'compile)
 (ido-mode t)
 (menu-bar-mode -1)
-(normal-erase-is-backspace-mode 1)
+(normal-erase-is-backspace-mode 0)
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (setq column-number-mode t)
@@ -75,3 +75,9 @@
 (require 'jade-mode)    
 (add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+;; ---------------------------
+;; -- Fix the Node REPL     --
+;; ---------------------------
+
+(setenv "NODE_NO_READLINE" "1")
